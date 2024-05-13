cc71e884cde8a391a2832b0da56a3a56d4a69d2a
baharkose <baharkse17@gmail.com>
2024-05-13 21:33:37 +0300

    feat: add basic files

---
+++ BlogAPI/index.js
@@ -0,0 +1,14 @@
+"use strict";
+/* -------------------------------------------------------
+             BLOG API
+------------------------------------------------------- */
+
+/*
+    cp .env-sample .env
+    npm init -y
+    npm i express dotenv mongoose express-async-errors
+    npm i morgan swagger-autogen swagger-ui-express redoc-express
+    mkdir logs
+    nodemon
+    https://www.toptal.com/developers/gitignore/
+*/
