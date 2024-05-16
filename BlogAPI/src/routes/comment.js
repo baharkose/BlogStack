63377cb48fb9a3da2794a676923e4c127fb0110f
baharkose <baharkse17@gmail.com>
2024-05-16 21:49:33 +0300

    feat: add comment permissions

--- BlogAPI/src/routes/comment.js
+++ BlogAPI/src/routes/comment.js
@@ -3,18 +3,20 @@
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
 const router = require("express").Router();
+// Call Permissions:
+const permissions = require("../middlewares/permissions");
 
 // Call Controllers:
 const comment = require("../controllers/comment");
 
 // comment
 // ------------------------------------------
-router.route("/").get(comment.list).post(comment.create);
+router.route("/").get(comment.list).post(permissions.isLogin, comment.create);
 
 router
   .route("/:commentId")
   .get(comment.read)
-  .put(comment.update)
-  .delete(comment.delete);
+  .put(permissions.isLogin, comment.update)
+  .delete(permissions.isLogin, comment.delete);
 // ------------------------------------------
 module.exports = router;
