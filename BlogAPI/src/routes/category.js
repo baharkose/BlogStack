2459899b2d492765f3ca3af7278ade429d832aa3
baharkose <baharkse17@gmail.com>
2024-05-16 21:47:39 +0300

    feat: add category permissions

--- BlogAPI/src/routes/category.js
+++ BlogAPI/src/routes/category.js
@@ -3,18 +3,20 @@
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
 const router = require("express").Router();
+// Call Permissions:
+const permissions = require("../middlewares/permissions");
 
 // Call Controllers:
 const category = require("../controllers/category");
 
 // comment
 // ------------------------------------------
-router.route("/").get(category.list).post(category.create);
+router.route("/").get(category.list).post(permissions.isAdmin, category.create);
 
 router
   .route("/:categoryId")
   .get(category.read)
-  .put(category.update)
-  .delete(category.delete);
+  .put(permissions.isAdmin, category.update)
+  .delete(permissions.isAdmin, category.delete);
 // ------------------------------------------
 module.exports = router;
